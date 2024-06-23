e756b918c93a2e764d89a4009fc92b9aca3dcec1
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-06-23 22:44:33 +0700

    add: nuxtlink

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,16 @@
 <template>
     <div>
+        <header>
+            <nav>
+                <h1>KongRuksiam Shop</h1>
+                <NuxtLink to="/">หน้าแรก</NuxtLink>
+                <NuxtLink to="/about">เกี่ยวกับเรา</NuxtLink>
+                <NuxtLink to="/products">สินค้าทั้งหมด</NuxtLink>
+            </nav>
+        </header>
+        <div>
         <h1>หน้าแรกของเว็บไซต์</h1>
+    </div>
     </div>
 </template>
 
