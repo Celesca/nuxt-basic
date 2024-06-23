52af6a99598b935f4f1056641ca3c39ae8279ef7
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-06-23 22:46:51 +0700

    add: styling navbar

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
     <div>
         <header>
             <nav>
-                <h1>KongRuksiam Shop</h1>
+                <h1 class="logo">KongRuksiam Shop</h1>
                 <NuxtLink to="/">หน้าแรก</NuxtLink>
                 <NuxtLink to="/about">เกี่ยวกับเรา</NuxtLink>
                 <NuxtLink to="/products">สินค้าทั้งหมด</NuxtLink>
@@ -19,5 +19,34 @@
 </script>
 
 <style scoped>
+
+nav {
+    margin: 10px auto;
+    padding: 10px 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: #333;
+}
+
+nav a {
+    color: white;
+    text-decoration: none;
+    margin-left: 12px;
+    font-size: 18px;
+    padding: 10px;
+}
+
+nav a:hover {
+    color: orange;
+}
+
+.logo {
+    margin-right: auto;
+    padding-left: 10px;
+    color: white;
+}
+
+
 
 </style>
