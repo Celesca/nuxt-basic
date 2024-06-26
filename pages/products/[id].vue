a11cbec52304bc500fb1f44645a8988aff206045
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-06-26 22:14:26 +0700

    add: dynamic routing product

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -1,13 +1,33 @@
 <template>
     <div>
-        <h1>รายละเอียดสินค้าชิ้นที่ {{ id }}</h1>
+        <div class="container">
+             <img :src="product.thumbnail" :alt="product.title">
+             <div class="detail">
+                    <h1>Name : {{ product.title }}</h1>
+                    <h2>Price : ${{ product.price }}</h2>
+                    <h2>Category : {{ product.category }}</h2>
+                    <h3>Brand : {{ product.brand }}</h3>
+                    <h4>Description : {{ product.description }}</h4>
+                </div>
+            </div>
     </div>
 </template>
 
 <script setup>
     const {id} = useRoute().params
+    const { data: product } = useFetch('https://dummyjson.com/products/' + id)
+    console.log(product)
 </script>
 
 <style scoped>
+
+.container {
+    display: flex;
+    justify-content: center;
+}
+
+.detail {
+    padding-left: 10px;
+}
 
 </style>
